8b57c512623f743e543e207f651c91586079844a
Rodrigo <rodrigosantiagosilva86@gmail.com>
2025-07-08 16:54:33 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- views/tarefa/listar.php
+++ views/tarefa/listar.php
@@ -1,18 +1,28 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 <h1>
     <?php echo $usuario['nome'] ?? ''?>
     <a id="btn_logout" href="/logout">Sair</a>
 </h1>
+
+    <p>
+        <a href="/tarefa/incluir">Adicionar</a><i class="fa-solid fa-circle-plus"></i><i class="fa-solid fa-trash-can"></i>
+    </p>
 <table>
     <thead>
         <tr>
-            <td>id</td> <td>titulo</td> <td>descricao</td>
+            <td>id</td> <td>titulo</td> <td>descricao</td> <td>Ação</td>
         </tr>
     </thead>
 <tbody>
     <?php
 foreach($tarefas as $tarefa){
-    echo "<tr> <td>{$tarefa['id']}</td> <td>{$tarefa['titulo']}</td>
-    <td>{$tarefa['descricao']}</td> </tr>";
+    echo "<tr>
+    <td>{$tarefa['id']}</td>
+    <td>{$tarefa['titulo']}</td>
+    <td>{$tarefa['descricao']}</td> 
+    <td><i class='fa-solid fa-circle-plus'></i></td>
+    <td><i class='fa-solid fa-trash-can'></i></td>
+    </tr>";
 }
 
 
